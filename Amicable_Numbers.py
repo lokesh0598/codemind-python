6b84abf0a93cdd248e867167b90911c3fcee7351
Lokesh Kona <82692617+lokesh0598@users.noreply.github.com>
2022-11-05 21:15:57 +0530

    n=int(input()) m=int(input()) s=0 p=0 for i in range(1,(n//2)+1):     if n%i==0:         s+=i for i in range(1,(m//2)+1):     if m%i==0:         p+=i if p==n and s==m:     print("Amicable") else:     print("Not Amicable")

--- Amicable_Numbers.py
+++ Amicable_Numbers.py
@@ -1,14 +1,14 @@
 n=int(input())
 m=int(input())
-sum=0
-for i in range(1,n):
+s=0
+p=0
+for i in range(1,(n//2)+1):
     if n%i==0:
-        sum+=i
-add=0
-for j in range(1,m):
-    if m%j==0:
-        add+=j
-if (sum==m) or (add==n):
+        s+=i
+for i in range(1,(m//2)+1):
+    if m%i==0:
+        p+=i
+if p==n and s==m:
     print("Amicable")
 else:
     print("Not Amicable")
